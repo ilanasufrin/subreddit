5e711802b0ac2bf2d05a46386ea88ce71bc5b2f5
ilanasufrin <ilanasufrin@gmail.com>
2014-07-03 13:17:44 -0400

    Update subreddit_generator_with_link.rb
    fixed indentation

--- lib/subreddit_generator_with_link.rb
+++ lib/subreddit_generator_with_link.rb
@@ -2,28 +2,28 @@ require 'mechanize'
 
 class Subreddit
 
-def random_subreddit
+  def random_subreddit
 
-mechanize = Mechanize.new
+    mechanize = Mechanize.new
 
-page = mechanize.get('http://www.reddit.com/')
+    page = mechanize.get('http://www.reddit.com/')
 
-link = page.link_with(text: 'random subreddit')
+    link = page.link_with(text: 'random subreddit')
 
-page = link.click
+    page = link.click
 
-first_link = page.search '/html/body/div/div/div/div/p/a'
+    first_link = page.search '/html/body/div/div/div/div/p/a'
 
-begin 
- first_link_parsed = first_link.first.text.strip
-rescue
-  random_subreddit
-else
-puts page.uri
-puts first_link_parsed
-end
+      begin 
+        first_link_parsed = first_link.first.text.strip
+      rescue
+        random_subreddit
+      else
+        puts page.uri
+        puts first_link_parsed
+      end
 
-end
+  end
 
 
 
